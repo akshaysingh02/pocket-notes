1f7bbade19549cb850fdd1eb384eb119a7c52493
Akshay singh <ww.akshay@gmail.com>
2024-03-11 17:22:09 +0530

    Fixed initial bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css';
+import GroupList from './components/GroupList';
+import NoteTakingArea from './components/NoteTakingArea';
 
 function App() {
   return (
     <div className="App">
-      <div className="left-wrapper">
-        <div className="heading-wrapper"><h1>Pocket Notes</h1></div>
-          <div className="groups-wrapper"></div>
-      </div>
-      <div className="right-wrapper">
-        image
-      </div>
+      <GroupList />
+      <NoteTakingArea />
     </div>
   );
 }
---
+++ src/components/GroupList.jsx
@@ -0,0 +1,10 @@
+function GroupList() {
+    return ( <>
+        <div className="left-wrapper">
+        <div className="heading-wrapper"><h1>Pocket Notes</h1></div>
+          <div className="groups-wrapper"></div>
+      </div>
+    </> );
+}
+
+export default GroupList;
