51572bdd0aca9e3a634e6662493edc6e4a8b5554
Akshay singh <ww.akshay@gmail.com>
2024-03-11 16:20:35 +0530

    Added structure and started the project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="left-wrapper">
+        <div className="heading-wrapper"><h1>Pocket Notes</h1></div>
+          <div className="groups-wrapper"></div>
+      </div>
+      <div className="right-wrapper">
+        image
+      </div>
+    </div>
+  );
+}
+
+export default App;
